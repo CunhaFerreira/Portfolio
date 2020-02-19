88139d8e0a1aa9c313beaf3a5f3c28b017c06531
Paola <47130293+CunhaFerreira@users.noreply.github.com>
2020-02-19 21:37:24 +1300

    Delete javascript.js

--- js/javascript.js
+++
@@ -1,6 +0,0 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    $(".home-inner img").css({
-        transform: 'translate3d(0, -' + (scroll / 10) + '%, 0) scale(' + (100 + scroll / 8) / 100 + ')',
-    });
-});
